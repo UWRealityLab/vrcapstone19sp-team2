b26255e6eacfc3f7af59ddd67c9305d9143c3bc0
shanks <ys74@cs.washington.edu>
2019-05-02 14:46:24 -0700

    flash light switch update

---
+++ escape_room/Assets/Scripts/FlashLightSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.input
+
+namespace Valve.VR.InteractionSystem {
+    public class FlashLightSwitch : MonoBehaviour
+    {
+        public SteamVR_Input_Sources RightHandInputSource = SteamVR_Input_Sources.RightHand;
+        // Update is called once per frame
+        private void OnAttachedToHand(Hand hand)
+        {
+            if (SteamVR_Actions._default.Squeeze.GetAxis(RightHandInputSource).Equals(1))
+            {
+                Light light = this.GetComponentInChildren<Light>();
+                if (light.enabled)
+                {
+                    light.enabled = false;
+                } else
+                {
+                    light.enabled = true;
+                }
+                //light.enabled = false;
+                Debug.Log("mmp");
+            }
+        }
+    }
+}
