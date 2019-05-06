1ed87aef96190eda394966086933f7331a6c19d8
shanks <ys74@cs.washington.edu>
2019-05-05 23:09:53 -0700

    Safe box mechanism good

--- escape_room/Assets/Scripts/SafeBoxPasscode.cs
+++ escape_room/Assets/Scripts/SafeBoxPasscode.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
+
 
 public class SafeBoxPasscode : MonoBehaviour
 {
+    public bool pass;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pass = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.GetComponent<LinearMapping>().value != 0)
+        {
+            pass = true;
+        }
+        Debug.Log("***" + pass);
     }
 }
