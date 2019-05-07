d7fa81f02d94230f48a0d36ae4bfb6979ef81204
Qian (Will) Yan <qiany7@cs.washington.edu>
2019-05-07 15:57:09 -0700

    Add the musicbox puzzle into main scene

---
+++ escape_room/Assets/Scripts/HandleTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class HandleTrigger : MonoBehaviour
+{
+    private Interactable interactable;
+    private bool open;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = this.GetComponent<Interactable>();
+        open = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MusicBoxOpen()
+    {
+        open = true;
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (open && interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            Debug.Log("Open");
+            GetComponentInParent<Animator>().SetTrigger("Play");
+            this.gameObject.AddComponent<IgnoreHovering>();
+        }
+    }
+}
