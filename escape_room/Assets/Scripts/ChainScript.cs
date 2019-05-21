1a9a8e09053e9f63343caa58b136ded99457f68d
Qian (Will) Yan <qiany7@cs.washington.edu>
2019-05-20 17:16:54 -0700

    Chain Interaction Script

---
+++ escape_room/Assets/Scripts/ChainScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ChainScript : MonoBehaviour
+{
+    public Transform ChainStart;
+    public Transform ChainEnd;
+    public GameObject Hand;
+    public float LengthOffset = 0.0f;
+    public bool enableHaptics = true;
+
+    // Haptics
+    public SteamVR_Action_Vibration hapticAction;
+
+    protected float MaxLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxLength = (ChainEnd.position - ChainStart.position).magnitude + LengthOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Hide Left Hand
+        Hand.GetComponent<Hand>().HideController(true);
+
+        Vector3 handDirection = (Hand.transform.position - ChainStart.position).normalized;
+        float handDistance = (Hand.transform.position - ChainStart.position).magnitude;
+
+        ChainEnd.position = ChainStart.position + handDirection * Mathf.Min(handDistance, MaxLength);
+
+        if (handDistance > MaxLength + LengthOffset)
+        {
+            Pulse();
+        }
+    }
+
+    public void Break()
+    {
+        // Show hand
+        Hand.GetComponent<Hand>().ShowController(true);
+        this.enabled = false;
+    }
+
+    private void Pulse()
+    {
+        hapticAction.Execute(0, 100f / 1000000f, 1000000f / 100f, 1, SteamVR_Input_Sources.LeftHand);
+    }
+}
