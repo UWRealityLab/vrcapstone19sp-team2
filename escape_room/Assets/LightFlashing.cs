3f5d4ae227b8ce850e50a44c5d309d54aef51895
Jiwen Wu <jiwenwumc@gmail.com>
2019-05-22 23:35:28 -0700

    added light flashing effect

---
+++ escape_room/Assets/LightFlashing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlashing : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject redLight, blueLight;
+
+    private float intensity;    
+    void Start()
+    {        
+        intensity = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        redLight.GetComponent<Light>().intensity = Mathf.Abs(Mathf.Sin(intensity)) * 1.5f;
+        blueLight.GetComponent<Light>().intensity = Mathf.Abs(Mathf.Cos(intensity)) * 1.5f;
+        intensity += 0.023f;
+        if(intensity >= 10000 * Mathf.PI)
+        {
+            intensity = 0f;
+        }
+    }
+    
+}
