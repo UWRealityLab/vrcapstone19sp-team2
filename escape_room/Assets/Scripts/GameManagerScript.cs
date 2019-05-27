841861a507d12174eb260d61cc8b81a611899189
Qian (Will) Yan <qiany7@cs.washington.edu>
2019-05-27 15:32:08 -0700

    Game manager

---
+++ escape_room/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UIContent;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public enum TaskTypes
+    {
+        RELEASE,
+        RADIO,
+        DESK,
+        FIND_FUSE,
+        FIND_MUSIC_BOX,
+        FIND_FLARE,
+        LIGHT,
+        KEY_HINT,
+        KEY,
+        ICE,
+        AMMO,
+        CURTAIN
+    }
+
+    public enum EventTypes
+    {
+        WAKE_UP,
+        CUTTER_CUT,
+    }
+
+    public HashSet<TaskTypes> completedTasks;
+    public HashSet<TaskTypes> activeTasks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        completedTasks = new HashSet<TaskTypes>();
+        activeTasks = new HashSet<TaskTypes>();
+    }
+
+    public void TriggerTask(TaskTypes task)
+    {
+        if (!completedTasks.Contains(task))
+        {
+            activeTasks.Add(task);
+        }
+    }
+
+    public void CompleteTask(TaskTypes task)
+    {
+        activeTasks.Remove(task);
+        completedTasks.Add(task);
+    }
+
+    public string GetTasks()
+    {
+        string tasks = "";
+        foreach (TaskTypes task in activeTasks)
+        {
+            tasks += TaskToUI[task] + "\n";
+        }
+        return tasks.Substring(tasks.Length - 1);
+    }
+}
---
+++ escape_room/Assets/Scripts/UIContent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GameManagerScript;
+
+public class UIContent : MonoBehaviour
+{
+    public static Hashtable TaskToUI = new Hashtable()
+    {
+        { TaskTypes.RELEASE, "Release yourself"},
+        { TaskTypes.RADIO, "Explore the radio"},
+        { TaskTypes.DESK, "Explore the desk"},
+        { TaskTypes.FIND_FUSE, "Find the fuse box"},
+        { TaskTypes.FIND_MUSIC_BOX, "Find the music box"},
+        { TaskTypes.FIND_FLARE, "Find the flare gun"},
+        { TaskTypes.LIGHT, "Recover the light"},
+        { TaskTypes.KEY_HINT, "Find hints about the key"},
+        { TaskTypes.KEY, "Find key"},
+        { TaskTypes.ICE, "Get the key out"},
+        { TaskTypes.AMMO, "Find more ammo"},
+        { TaskTypes.CURTAIN, "Open the curtain"}
+    };
+}
