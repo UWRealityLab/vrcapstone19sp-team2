e4cb286bfb0144a5c46806e3abec889ddfe5ee52
shanks <ys74@cs.washington.edu>
2019-05-28 17:31:26 -0700

    safe box changed to animation

--- escape_room/Assets/Scripts/SafeboxOpener.cs
+++ escape_room/Assets/Scripts/SafeboxOpener.cs
@@ -9,8 +9,11 @@ public class SafeboxOpener : MonoBehaviour
     public bool okToOpenDoor;
     GameObject sib;
     CircularDrive circularDrive;
+
+    private Interactable interactable;
     void Start()
     {
+        interactable = this.GetComponent<Interactable>();
         passcodeGood = false;
         circularDrive = this.GetComponent<CircularDrive>();
         //tryNumber = 1;
@@ -23,28 +26,51 @@ public class SafeboxOpener : MonoBehaviour
         //Debug.Log(sib.name);
 
         passcodeGood = sib.GetComponent<SafeBoxPasscode>().pass;
-        if (passcodeGood)
+        //if (passcodeGood)
+        //{
+        //    Destroy(GetComponent<IgnoreHovering>());
+        //}
+        //else if (!this.gameObject.GetComponent<IgnoreHovering>())
+        //{
+        //    this.gameObject.AddComponent<IgnoreHovering>();
+        //}
+    }
+
+    //private void LateUpdate()
+    //{
+    //    float rotation = float.Parse(this.GetComponent<LinearMapping>().value.ToString("0.00"));
+    //    // Debug.Log("opener rotation: " + this.gameObject.transform.localEulerAngles.z);
+
+    //    if (this.gameObject.transform.localEulerAngles.z > 90.0f && passcodeGood)
+    //    {
+    //        // TODO add open sound
+    //        okToOpenDoor = true;
+    //    } else
+    //    {
+    //        okToOpenDoor = false;
+    //    }
+    //}
+
+    //public void EnableOpener()
+    //{
+    //    Destroy(GetComponent<IgnoreHovering>());
+    //}
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (passcodeGood && interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
         {
-            Destroy(GetComponent<IgnoreHovering>());
-        }
-        else if (!this.gameObject.GetComponent<IgnoreHovering>())
+            GetComponent<Animator>().SetTrigger("open");
+        } else if (!passcodeGood && interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
         {
-            this.gameObject.AddComponent<IgnoreHovering>();
+            GetComponent<Animator>().SetTrigger("openFail");
         }
     }
 
-    private void LateUpdate()
+    public void EnableDoorOpen()
     {
-        float rotation = float.Parse(this.GetComponent<LinearMapping>().value.ToString("0.00"));
-        // Debug.Log("opener rotation: " + this.gameObject.transform.localEulerAngles.z);
-
-        if (this.gameObject.transform.localEulerAngles.z > 90.0f && passcodeGood)
-        {
-            // TODO add open sound
-            okToOpenDoor = true;
-        } else
-        {
-            okToOpenDoor = false;
-        }
+        okToOpenDoor = true;
     }
 }
