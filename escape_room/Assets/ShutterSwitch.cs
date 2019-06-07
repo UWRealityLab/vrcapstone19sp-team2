406b4a208b3b5211f28616c06d9d98bffe8096ef
Qian Yan <qiany7@cs.washington.edu>
2019-06-07 14:31:14 -0700

    quick fix

--- escape_room/Assets/ShutterSwitch.cs
+++ escape_room/Assets/ShutterSwitch.cs
@@ -33,8 +33,8 @@ public class ShutterSwitch : MonoBehaviour
 
         if (startingGrabType != GrabTypes.None)
         {           
-            shutter1.GetComponent<Animator>().SetTrigger(trigger);
-            // shutter2.GetComponent<Animator>().SetTrigger(trigger);
+            // shutter1.GetComponent<Animator>().SetTrigger(trigger);
+            shutter2.GetComponent<Animator>().SetTrigger(trigger);
 
             if (Mathf.Abs(switchObject.transform.localEulerAngles.x - onPosition.localEulerAngles.x) <= delta)
             {
